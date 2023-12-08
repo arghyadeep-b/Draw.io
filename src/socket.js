8019e2a3f0522897c53f0790bc9f24085578497f
Arghyadeep Bhowmick <bhowmickarghyadeep2@gmail.com>
2023-12-09 02:03:08 +0530

    Connected to live server

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
+const URL = process.env.NODE_ENV === 'production' ? 'https://draw-io-server.onrender.com/' : "http://localhost:5001";
 // same origin version
-export const socket = io("http://localhost:5001");
+export const socket = io(URL);
 // cross origin version
 // const socket = io("https://server-domain.com/admin");

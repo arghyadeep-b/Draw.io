766aa89e5fb48e5b5d6b895380f5de9e10dcf5d3
Arghyadeep Bhowmick <bhowmickarghyadeep2@gmail.com>
2023-12-08 17:28:14 +0530

    Communication between toolbox and menu established

--- src/components/Toolbox/index.js
+++ src/components/Toolbox/index.js
@@ -1,12 +1,15 @@
+import { useSelector } from 'react-redux';
 import styles from './index.module.css'
-import { COLORS } from '@/constants';
+import { COLORS, MENU_ITEMS } from '@/constants';
 
 const Toolbox = () => {
+    const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+
     const updateBrushSize = () => { };
 
     return (
         <div className={styles.toolboxContainer}>
-            <div className={styles.toolItem}>
+            {activeMenuItem === MENU_ITEMS.PENCIL && <div className={styles.toolItem} style={{ marginBottom: "1.25rem" }}>
                 <h4 className={styles.toolText}>Brush Color</h4>
                 <div className={styles.itemContainer}>
                     {/* div for color */}
@@ -17,10 +20,10 @@ const Toolbox = () => {
                     <div className={styles.colorBox} style={{ backgroundColor: COLORS.ORANGE }} />
                     <div className={styles.colorBox} style={{ backgroundColor: COLORS.YELLOW }} />
                 </div>
-            </div>
+            </div>}
             <div className={styles.toolItem}>
                 <h4 className={styles.toolText}>
-                    Brush Size
+                    {activeMenuItem === MENU_ITEMS.ERASER ? 'Eraser' : 'Brush'} Size
                 </h4>
                 <input type="range" min={1} max={10} step={1} onChange={updateBrushSize} />
             </div>

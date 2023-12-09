cc34652761dd3458c0518a13fd75fc335d80e411
Arghyadeep Bhowmick <bhowmickarghyadeep2@gmail.com>
2023-12-09 17:03:13 +0530

    Dynamic routing to '/username' added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 import Menu from "@/components/Menu";
 import Toolbox from "@/components/Toolbox";
 import Board from "@/components/Board";
@@ -5,9 +8,18 @@ import { UserButton, useUser } from "@clerk/nextjs";
 
 export default function Home() {
   const { isSignedIn, user, isLoaded } = useUser();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to /[username] if a username is available
+    if (isSignedIn) {
+      router.push(`/${user.username}`);
+    }
+  }, [isSignedIn, router]);
+
   return (
     <>
-      {console.log("user", user)}
       <Menu />
       <Toolbox />
       <Board />
